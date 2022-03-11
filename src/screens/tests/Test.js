c139afd64c16c6c26e6555e0311e8b74e2d07ea0
jamolina13 <88411328+jamolina13@users.noreply.github.com>
2022-03-11 14:16:10 -0500

    Update Test.js

--- src/screens/tests/Test.js
+++ src/screens/tests/Test.js
@@ -19,36 +19,55 @@ import Indice_Barthel from '../../../assets/img_sistema/Indice_Barthel.png';
 const { width: WIDTH } = Dimensions.get('window')
 
 export const Test = (props) => {
-const navigation = props.navigation;
-const params = props.route.params;
-  //static contextType = NavigationContext;
-  const [state, setstate]=useState({
+  const navigation = props.navigation;
+  const params = props.route.params;
+  //static contextType = NavigationContext;  
+   
+  var idYesavage= params.idYesavage  
+  var idLawton= params.idLawton
+  var idMini= params.idMini
+  var idBarthel= params.idBarthel
+  var validacioIdBarthel=false
+  var validacioIdLawton=false
+  var validacioIdMini=false
+  var validacioIdYesavage= false;
+  const [state, setState] = useState({
     isReady: false,
     listadoAdultMay: [],
-    enc_id:params.enc_id,
+    enc_id: params.enc_id,
     id: params.id,
-    
     nombre: params.nombre,
-    apellido: params.apellido,
+    apellido: params.apellido,      
+    
+    /* 
+    idBarthel: params.idBarthel,
+    idYesavage: params.idYesavage,
+    idLawton: params.idLawton,
+    idMini: params.idMini,
+    */
   });
-
-
-  
-
-  const TestBarthel = () => {
-    navigation.navigate('TestBarthel')
+  const IndiTestBarthel = () => {
+    console.log("Encabezado: "+state.enc_id)
+    navigation.navigate('TestBarthel', {
+      enc_id: state.enc_id,
+    });
+  }
+  const IndiTestMiniExamenMental = () => {    
+    navigation.navigate('TestMiniExamenMental', {
+      enc_id: state.enc_id,
+    });
   }
 
-  const TestMiniExamenMental = () => {
-    navigation.navigate('TestMiniExamenMental')
+  const IndiTestLawtonBrody = () => {    
+    navigation.navigate('IndiTestLawtonBrody', {
+      enc_id: state.enc_id,
+    });
   }
 
-  const IndiTestLawtonBrody = () => {
-    navigation.navigate('IndiTestLawtonBrody')
-  }
-
-  const IndiTestYesavage = () => {
-    navigation.navigate('IndiTestYesavage')
+  const IndiTestYesavage = () => {    
+    navigation.navigate('IndiTestYesavage', {
+      enc_id: state.enc_id,
+    });
   }
 
   const Observaciones = () => {
@@ -59,78 +78,114 @@ const params = props.route.params;
     navigation.navigate('Encabezado')
   }
 
-  return (
-  <Content padder >
-    <View>
-      <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>
-        Adulto Mayor: {' ' + state.nombre + ' ' + state.apellido}</Text>
-        <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>
-        encabezado id: {' ' + state.enc_id }</Text>
-        
-      <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
-        <View style={{ alignItems: 'center', padding: 20 }}>
-          <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} 
-          onPress={IndiTestYesavage}>
-            <View style={{ alignItems: 'center' }}>
-              <Thumbnail square source={Escala_Yesavage} style={{ width: 125, height: 125 }} />
-            </View>
-          </TouchableOpacity>
-          <View style={{ alignItems: 'center' }}>
-            <Text>ESCALA DE</Text>
-            <Text>YESAVAGE</Text>
-          </View>
-        </View>
-        <View style={{ alignItems: 'center', padding: 20 }}>
-          <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} onPress={TestBarthel}>
-            <View style={{ alignItems: 'center' }}>
-              <Thumbnail square source={Indice_Barthel} style={{ width: 125, height: 125 }} />
-            </View>
-          </TouchableOpacity >
-          <View style={{ alignItems: 'center' }}>
-            <Text>INDICE DE</Text>
-            <Text>BARTHEL</Text>
-          </View>
-        </View>
-      </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <View style={{ alignItems: 'center', paddingRight: 20, paddingLeft: 20 }}>
-          <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} onPress={IndiTestLawtonBrody}>
-            <View style={{ alignItems: 'center' }}>
-              <Thumbnail square source={Escala_Lawton_Brody} style={{ width: 125, height: 125 }} />
-            </View>
-          </TouchableOpacity>
-          <View style={{ alignItems: 'center' }}>
-            <Text>ESCALA DE</Text>
-            <Text>LAWTON Y BRODY</Text>
-          </View>
-        </View>
-        <View style={{ alignItems: 'center', paddingRight: 20, paddingLeft: 20 }}>
-          <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} onPress={TestMiniExamenMental}>
-            <View style={{ alignItems: 'center' }}>
-              <Thumbnail square source={Mini_Examen_Mental} style={{ width: 125, height: 125 }} />
-            </View>
-          </TouchableOpacity>
-          <View style={{ alignItems: 'center' }}>
-            <Text>MINI EXAMEN DEL</Text>
-            <Text>ESTADO MENTAL</Text>
-          </View>
-        </View>
-      </View>
 
-      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <View>
-          <TouchableOpacity style={styles.btnContinuar} onPress={Observaciones}>
-            <Text style={styles.textBtn}>AGREGAR</Text>
-            <Text style={styles.textBtn}>OBSERVACIÓN</Text>
-          </TouchableOpacity>
-          { <TouchableOpacity style={styles.btnContinuar}>
-            <Text style={styles.textBtn}>VER</Text>
-            <Text style={styles.textBtn}>RESULTADOS</Text>
-          </TouchableOpacity> }
+  const validateBarthel = () => {
+    if(idBarthel!=undefined){
+      validacioIdBarthel=true;     
+      return validacioIdBarthel
+    }
+  }
+
+  const validateYesavage = () => {
+    if(idYesavage!=undefined){
+      validacioIdYesavage=true;     
+      return validacioIdYesavage
+    }
+  }
+ 
+  const validateLawton = () => {
+    if(idLawton!=undefined){
+      validacioIdLawton=true;     
+      return validacioIdLawton
+    }
+  }
+
+  const validateMini = () => {
+    if(idMini!=undefined){
+      validacioIdMini=true;     
+      return validacioIdMini
+    }
+  }
+
+  return (
+    <Content padder >
+      <View>
+        <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>
+          Adulto Mayor: {' ' + state.nombre + ' ' + state.apellido}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>
+          encabezado id: {' ' + state.enc_id}</Text>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+          <View style={{ alignItems: 'center', padding: 20 }}>
+            <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }}
+              disabled={validateYesavage()}
+              onPress={IndiTestYesavage}>
+              <View style={{ alignItems: 'center' }}>
+                <Thumbnail square source={Escala_Yesavage} style={{ width: 125, height: 125 }} />
+              </View>
+            </TouchableOpacity>
+            <View style={{ alignItems: 'center' }}>
+              <Text>ESCALA DE</Text>
+              <Text>YESAVAGE</Text>
+            </View>
+          </View>
+          <View style={{ alignItems: 'center', padding: 20 }}>
+            <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }}
+            disabled={validateBarthel()} 
+            onPress={IndiTestBarthel}>
+              <View style={{ alignItems: 'center' }}>
+                <Thumbnail square source={Indice_Barthel} style={{ width: 125, height: 125 }} />
+              </View>
+            </TouchableOpacity >
+            <View style={{ alignItems: 'center' }}>
+              <Text>INDICE DE</Text>
+              <Text>BARTHEL</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <View style={{ alignItems: 'center', paddingRight: 20, paddingLeft: 20 }}>
+            <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} 
+            disabled={validateLawton()}
+            onPress={IndiTestLawtonBrody}>
+              <View style={{ alignItems: 'center' }}>
+                <Thumbnail square source={Escala_Lawton_Brody} style={{ width: 125, height: 125 }} />
+              </View>
+            </TouchableOpacity>
+            <View style={{ alignItems: 'center' }}>
+              <Text>ESCALA DE</Text>
+              <Text>LAWTON Y BRODY</Text>
+            </View>
+          </View>
+          <View style={{ alignItems: 'center', paddingRight: 20, paddingLeft: 20 }}>
+            <TouchableOpacity style={{ width: 125, height: 125, borderRadius: 90, justifyContent: 'center' }} 
+            disabled={validateMini()}
+            onPress={IndiTestMiniExamenMental}>
+              <View style={{ alignItems: 'center' }}>
+                <Thumbnail square source={Mini_Examen_Mental} style={{ width: 125, height: 125 }} />
+              </View>
+            </TouchableOpacity>
+            <View style={{ alignItems: 'center' }}>
+              <Text>MINI EXAMEN DEL</Text>
+              <Text>ESTADO MENTAL</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <View>
+            <TouchableOpacity style={styles.btnContinuar} onPress={Observaciones}>
+              <Text style={styles.textBtn}>AGREGAR</Text>
+              <Text style={styles.textBtn}>OBSERVACIÓN</Text>
+            </TouchableOpacity>
+            {<TouchableOpacity style={styles.btnContinuar}>
+              <Text style={styles.textBtn}>VER</Text>
+              <Text style={styles.textBtn}>RESULTADOS</Text>
+            </TouchableOpacity>}
+          </View>
         </View>
       </View>
-    </View>
-  </Content>
+    </Content>
   );
 }
 
